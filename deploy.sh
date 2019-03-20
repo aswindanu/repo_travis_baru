94abc9426031ffa2ec461cc27bf44aae8f678423
aswindanu <aswin@alphatech.id>
2019-03-20 17:59:20 +0700

    Bisa dong

--- deploy.sh
+++ deploy.sh
@@ -9,5 +9,5 @@ source ~/.profile
 echo "$DOCKERHUB_PASS" | sudo docker login --username $DOCKERHUB_USER --password-stdin
 sudo docker stop hello4
 sudo docker rm hello4
-sudo docker rmi blasterb0y/helloworld
-sudo docker run -d --name hello4 -p 5000:5000 blasterb0y/helloworld:latest
+sudo docker rmi blasterb0y/hello4
+sudo docker run -d --name hello4 -p 5000:5000 blasterb0y/hello4:latest
